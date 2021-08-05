43a3e9e53b5ff628f6055307acd7a1d9332d5f96
sy109 <s-yoon3@wiu.edu>
2021-08-05 21:30:26 +0900

    스프링 빈등록 중복 등록과 충돌

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -19,7 +19,7 @@ public class ComponentFilterAppConfigTest {
         ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
         BeanA beanA = ac.getBean("beanA", BeanA.class);
         assertThat(beanA).isNotNull();
-        
+
         assertThrows(
                 NoSuchBeanDefinitionException.class, () -> ac.getBean("beanB", BeanB.class)
         );
